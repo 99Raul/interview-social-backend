a1e474349c112460b2262f218e4d758b429c0a12
99Raul <rauljr96@gmail.com>
2020-06-10 13:00:48 -0700

    added a comments model in posts

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
-# from profiles.models import Profile
+from profiles.models import Profile
 # Create your models here.
 
 class Post(models.Model):
@@ -22,3 +22,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-created',)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(max_length=300, default="blank")
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.user)
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,4 +5,4 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['first_name', 'last_name', 'user','email','friends']
+        fields = ['first_name', 'last_name', 'user','email','friends','country']
