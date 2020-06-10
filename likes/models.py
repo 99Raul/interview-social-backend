28056f965640d0ba9e33d36a27876d1551956b52
99Raul <rauljr96@gmail.com>
2020-06-10 12:13:32 -0700

    added a likes model

---
+++ likes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from interview.models import Post
+# Create your models here.
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='LIKE', max_length =8)
+
+
+    def __str__(self):
+        return f"{self.user}-{self.post}-{self.value}"
